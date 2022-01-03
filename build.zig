b77453146b36299ace10da06f2d54264e0b3d376
Vincent Rischmann <vincent@rischmann.fr>
2022-01-04 00:58:42 +0100

    build: add a macOS cross-compilation test target
    The tests _may_ run with darling with `zig build -fdarling test`, but I
    haven't tested that yet.

--- build.zig
+++ build.zig
@@ -110,6 +110,21 @@ const all_test_targets = switch (builtin.target.cpu.arch) {
                 },
                 .bundled = true,
             },
+            // Targets macOS
+            TestTarget{
+                .target = .{
+                    .cpu_arch = .x86_64,
+                    .os_tag = .macos,
+                },
+                .bundled = true,
+            },
+            TestTarget{
+                .target = .{
+                    .cpu_arch = .aarch64,
+                    .os_tag = .macos,
+                },
+                .bundled = true,
+            },
         },
         .windows => [_]TestTarget{
             TestTarget{
